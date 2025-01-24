3c7bb762f6fe8484c30eaa4d2af613fe451295d9
Martin Echeverria <martin.echeverria@accenture.com>
2025-01-24 01:16:20 -0300

    fix: improve total time calculation and display timers based on phase

--- src/app/pages/structures/tab2/components/training-structure/training-structure.component.ts
+++ src/app/pages/structures/tab2/components/training-structure/training-structure.component.ts
@@ -26,17 +26,36 @@ export class TrainingStructureComponent implements OnInit {
   public restBetweenSeriesMinutes: number;
   public restBetweenSeriesSeconds: number;
 
+  public currentRound: number;
+  public currentSeries: number;
+  public currentPhase: string;
+  public isRunning: boolean;
+  public isPaused: boolean;
+
+  private intervalId: any;
+
   constructor(
     private sqliteService: SqlliteManagerService,
     private translateService: TranslateService,
   ) {
     this.structureSelected = new Structure();
     this.totalTime = 0;
+    this.currentRound = 0;
+    this.currentSeries = 0;
+    this.currentPhase = 'Preparation';
+    this.isRunning = false;
+    this.isPaused = false;
   }
 
   ngOnInit() {
     this.getTrainingStructure(this.structureId);
 
+  }
+
+  resetStructure() {
+    this.currentPhase = 'Preparation';
+    this.isRunning = false;
+    this.getTrainingStructure(this.structureId);
   }
 
   getTrainingStructure(id: number) {
@@ -51,12 +70,22 @@ export class TrainingStructureComponent implements OnInit {
   }
 
   calculateTotal() {
-    this.totalTime = this.structureSelected.PreparationTime + (
-      (this.structureSelected.TrainingTime +
-        this.structureSelected.RestTime) *
-      this.structureSelected.Rounds +
-      this.structureSelected.RestBetweenSeries
-    ) * this.structureSelected.Series;
+    const preparationTime = this.structureSelected.PreparationTime;
+
+    const trainingAndRestTimePerRound =
+      (this.structureSelected.TrainingTime + this.structureSelected.RestTime) *
+      (this.structureSelected.Rounds - 1); // Entrenamiento y descanso en todas menos la última ronda
+
+    const trainingTimeLastRound = this.structureSelected.TrainingTime; // Entrenamiento de la última ronda
+
+    const totalSeriesTime =
+      (trainingAndRestTimePerRound + trainingTimeLastRound) * this.structureSelected.Series; // Tiempo total por serie
+
+    const restBetweenSeriesTime =
+      this.structureSelected.RestBetweenSeries * (this.structureSelected.Series - 1); // Excluye el descanso después de la última serie
+
+    // Total = Preparación + Tiempo total de las series + Tiempos entre series
+    this.totalTime = preparationTime + totalSeriesTime + restBetweenSeriesTime;
   }
 
   convertSecondsToMinutesAndSeconds(totalSeconds: number) {
@@ -88,5 +117,128 @@ export class TrainingStructureComponent implements OnInit {
     this.restBetweenSeriesMinutes = restBetweenSeriesConverted.minutes;
     this.restBetweenSeriesSeconds = restBetweenSeriesConverted.seconds;
   }
+
+  showTime(time: number, phase: string) {
+    if (this.isRunning) {
+      if (phase === 'Preparation') {
+        const preparationTimeConverted = this.convertSecondsToMinutesAndSeconds(time);
+        this.preparationTimeMinutes = preparationTimeConverted.minutes;
+        this.preparationTimeSeconds = preparationTimeConverted.seconds;
+      } else if (phase === 'Training') {
+        const trainingTimeConverted = this.convertSecondsToMinutesAndSeconds(time);
+        this.trainingTimeMinutes = trainingTimeConverted.minutes;
+        this.trainingTimeSeconds = trainingTimeConverted.seconds;
+      } else if (phase === 'Rest') {
+        const restTimeConverted = this.convertSecondsToMinutesAndSeconds(time);
+        this.restTimeMinutes = restTimeConverted.minutes;
+        this.restTimeSeconds = restTimeConverted.seconds;
+      } else if (phase === 'RestBetweenSeries') {
+        const restBetweenSeriesConverted = this.convertSecondsToMinutesAndSeconds(time);
+        this.restBetweenSeriesMinutes = restBetweenSeriesConverted.minutes;
+        this.restBetweenSeriesSeconds = restBetweenSeriesConverted.seconds;
+      }
+    }
+  }
+
+
+  startTimers() {
+    this.currentRound = 1;
+    this.currentSeries = 1;
+    this.startPreparationTimer();
+  }
+
+  startPreparationTimer() {
+    this.currentPhase = 'Preparation';
+    let preparationTime = this.structureSelected.PreparationTime;
+    this.intervalId = setInterval(() => {
+      // Código existente para manejar la cuenta regresiva
+      preparationTime--;
+      if (preparationTime < 0)
+        this.showTime(0, this.currentPhase);
+      else
+        this.showTime(preparationTime, this.currentPhase);
+      if (preparationTime < 0) {
+        clearInterval(this.intervalId);
+        this.startTrainingTimer();
+      }
+    }, 1000);
+  }
+
+  startTrainingTimer() {
+    this.currentPhase = 'Training';
+    let trainingTime = this.structureSelected.TrainingTime;
+    this.intervalId = setInterval(() => {
+      // Código existente para manejar la cuenta regresiva
+      trainingTime--;
+      if (trainingTime < 0)
+        this.showTime(this.structureSelected.TrainingTime, this.currentPhase);
+      else
+        this.showTime(trainingTime, this.currentPhase);
+      if (trainingTime < 0 && this.currentRound < this.structureSelected.Rounds) {
+        clearInterval(this.intervalId);
+        this.startRestTimer();
+      } else if (trainingTime < 0 && this.currentRound === this.structureSelected.Rounds && this.currentSeries != this.structureSelected.Series) {
+        clearInterval(this.intervalId);
+        this.startRestBetweenSeriesTimer();
+      } else if (trainingTime < 0 && this.currentRound === this.structureSelected.Rounds && this.currentSeries === this.structureSelected.Series) {
+        clearInterval(this.intervalId);
+        this.currentSeries = 0;
+        this.currentRound = 0;
+        this.currentPhase = '';
+        this.resetStructure();
+      }
+    }, 1000);
+  }
+
+  startRestTimer() {
+    this.currentPhase = 'Rest';
+    let restTime = this.structureSelected.RestTime;
+    this.intervalId = setInterval(() => {
+      // Código existente para manejar la cuenta regresiva
+      restTime--;
+      if (restTime < 0)
+        this.showTime(this.structureSelected.RestTime, this.currentPhase);
+      else
+        this.showTime(restTime, this.currentPhase);
+      if (restTime < 0 && this.currentRound < this.structureSelected.Rounds) {
+        clearInterval(this.intervalId);
+        this.currentRound++;
+        this.startTrainingTimer();
+      } else if (restTime < 0 && this.currentRound === this.structureSelected.Rounds) {
+        clearInterval(this.intervalId);
+        this.startRestBetweenSeriesTimer();
+      }
+    }, 1000);
+  }
+
+  startRestBetweenSeriesTimer() {
+    this.currentPhase = 'RestBetweenSeries';
+    let restBetweenSeriesTime = this.structureSelected.RestBetweenSeries;
+    this.intervalId = setInterval(() => {
+      // Código existente para manejar la cuenta regresiva
+      restBetweenSeriesTime--;
+      if (restBetweenSeriesTime < 0)
+        this.showTime(this.structureSelected.RestBetweenSeries, this.currentPhase);
+      else
+        this.showTime(restBetweenSeriesTime, this.currentPhase);
+      if (restBetweenSeriesTime < 0 && this.currentSeries < this.structureSelected.Series) {
+        clearInterval(this.intervalId);
+        this.currentSeries++;
+        this.currentRound = 1;
+        this.startTrainingTimer();
+      }
+    }, 1000);
+  }
+
+  toggleTimer() {
+    if (!this.isRunning) {
+      this.isRunning = true;
+      this.startTimers();
+    }
+    else {
+      this.isRunning = false;
+    }
+  }
+
 
 }
