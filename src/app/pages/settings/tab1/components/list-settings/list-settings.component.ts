784f8fd720ca1b5c7fdab8cbe299e7e6399725d9
Martin Echeverria <martin.echeverria@accenture.com>
2025-03-24 21:22:46 -0300

    Fix detect changes in settings

--- src/app/pages/settings/tab1/components/list-settings/list-settings.component.ts
+++ src/app/pages/settings/tab1/components/list-settings/list-settings.component.ts
@@ -9,46 +9,50 @@ import { SqlliteManagerService } from 'src/app/services/sqllite-manager.service'
   styleUrls: ['./list-settings.component.scss'],
   standalone: false
 })
-export class ListSettingsComponent  implements OnInit {
+export class ListSettingsComponent implements OnInit {
 
   public configuration: Configuration[];
-    public showForm: boolean;
-  
-    constructor(
-      private sqliteService: SqlliteManagerService,
-      private soundService: SoundService,
-    ) { 
-      this.showForm = false;
-      this.configuration = [];
-    }
-  
-    ngOnInit() {
-      Promise.all([
+  public showForm: boolean;
+
+  constructor(
+    private sqliteService: SqlliteManagerService,
+    private soundService: SoundService,
+  ) {
+    this.showForm = false;
+    this.configuration = [];
+  }
+
+  ngOnInit() {
+    this.loadConfiguration();
+  }
+
+  loadConfiguration() {
+    Promise.all([
       this.sqliteService.getConfiguration()]).then((data) => {
         this.configuration = data[0];
       });
-    }
+  }
 
-    pinFormatter(value: number) {
-      if(value == 0){
-        return '0';
-      }
-      return `${value}0`;
+  pinFormatter(value: number) {
+    if (value == 0) {
+      return '0';
     }
+    return `${value}0`;
+  }
 
-    getConfiguration() {
-      return this.configuration[0];
-    }
+  getConfiguration() {
+    return this.configuration[0];
+  }
 
-    testSound(event: any) {
-      const selectedSound = event.detail.value;
-      this.soundService.testSound(selectedSound);
-    }
+  testSound(event: any) {
+    const selectedSound = event.detail.value;
+    this.soundService.testSound(selectedSound);
+  }
 
-    testVolumen(event: any){
-      const selectedVolume = event.detail.value;
-      const selectedSound = this.configuration[0].BeepSoundSelected;
-      this.soundService.testVolumen(selectedVolume, selectedSound);
-    }
+  testVolumen(event: any) {
+    const selectedVolume = event.detail.value;
+    const selectedSound = this.configuration[0].BeepSoundSelected;
+    this.soundService.testVolumen(selectedVolume, selectedSound);
+  }
 
 }
--- src/app/pages/settings/tab1/tab1.page.ts
+++ src/app/pages/settings/tab1/tab1.page.ts
@@ -3,6 +3,8 @@ import { TranslateService } from '@ngx-translate/core';
 import { AlertService } from 'src/app/services/alert.service';
 import { SqlliteManagerService } from 'src/app/services/sqllite-manager.service';
 import { ListSettingsComponent } from './components/list-settings/list-settings.component';
+import { IonRouterOutlet, NavController } from '@ionic/angular';
+import { Configuration } from 'src/app/models/configuration';
 
 @Component({
   selector: 'app-tab1',
@@ -14,11 +16,53 @@ export class Tab1Page {
 
   @ViewChild(ListSettingsComponent) listSettings!: ListSettingsComponent;
 
+  private initialConfiguration: Configuration; // Configuración inicial para comparar cambios
+  public hasUnsavedChanges: boolean = false; // Estado para rastrear cambios
+
+
   constructor(
     private sqliteService: SqlliteManagerService,
     private translateService: TranslateService,
     private alertService: AlertService,
-  ) {}
+    private navController: NavController,
+  ) { }
+
+  loadConfiguration() {
+    this.sqliteService.getConfiguration().then(config => {
+      this.initialConfiguration = this.normalizeConfig(config[0]); // Crear copia inicial
+    });
+  }
+
+  ionViewWillEnter() {
+    this.listSettings.loadConfiguration(); // Cargar configuración actual
+    // Cargar configuración inicial y guardar copia
+    this.loadConfiguration();
+  }
+
+  ionViewWillLeave(event: Event) {
+    this.onSettingsChange();
+    if (this.hasUnsavedChanges) {
+      this.alertService.alertConfirm(
+        this.translateService.instant('label.settings'),
+        this.translateService.instant('label.settings.unsaved'),
+        () => {
+          this.navController.back();
+          this.hasUnsavedChanges = false;
+        }
+      );
+    }
+  }
+
+  // Método llamado cuando el usuario hace algún cambio
+  onSettingsChange() {
+    this.checkForUnsavedChanges();
+  }
+
+  // Comparar configuración actual con la inicial
+  private checkForUnsavedChanges() {
+    const currentConfig = this.normalizeConfig(this.listSettings.getConfiguration());
+    this.hasUnsavedChanges = JSON.stringify(this.initialConfiguration) !== JSON.stringify(currentConfig);
+  }
 
   onSave() {
     const self = this;
@@ -32,11 +76,13 @@ export class Tab1Page {
   }
 
   updateConfiguration() {
-    this.sqliteService.updateConfiguration(this.listSettings.getConfiguration()).then(() => {
+    this.sqliteService.updateConfiguration(this.listSettings.getConfiguration()).then((data) => {
       this.alertService.alertMessage(
         this.translateService.instant('label.success'),
         this.translateService.instant('label.success.message.edit.configuration')
       );
+      this.hasUnsavedChanges = false;
+      this.loadConfiguration();
     }).catch(error => {
       this.alertService.alertMessage(
         this.translateService.instant('label.error'),
@@ -44,5 +90,28 @@ export class Tab1Page {
       );
     });
   }
+
+  private normalizeConfig(config: Configuration): Configuration {
+    const normalizedConfig: any = {};
+
+    for (const key in config) {
+      if (key === 'Id' || key === 'SoundVolume') {
+        // Excluir 'Id' y 'SoundVolume', dejarlos tal cual
+        normalizedConfig[key] = config[key];
+      } else if (config[key] === 0 || config[key] === 1) {
+        // Convertir 0 y 1 en false o true
+        normalizedConfig[key] = !!config[key];
+      } else if (typeof config[key] === 'boolean') {
+        // Si ya es booleano, mantenerlo
+        normalizedConfig[key] = config[key];
+      } else {
+        // Mantener el resto igual
+        normalizedConfig[key] = config[key];
+      }
+    }
+
+    return normalizedConfig as Configuration;
+  }
+
 
 }
