9d65debd1efdda56382529a48e5e2aadc4413168
Martin Echeverria <martin.echeverria@accenture.com>
2025-05-02 05:17:27 -0300

    Fix sizes in tab

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'Manage classes',
+  appName: 'Trainning Time',
   webDir: 'www',
   plugins: {
     CapacitorSQLite: {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { Platform } from '@ionic/angular';
 import { TranslateService } from '@ngx-translate/core';
 import { SqlliteManagerService } from './services/sqllite-manager.service';
 import { AlertService } from './services/alert.service';
+import { App as CapacitorApp } from '@capacitor/app';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -16,9 +18,10 @@ export class AppComponent {
   public isWeb: boolean;
   public load: boolean;
 
-  constructor( 
+  constructor(
     private translate: TranslateService,
     private platform: Platform,
+    private router: Router,
     private sqliteService: SqlliteManagerService,
     private alertServie: AlertService
   ) {
@@ -29,26 +32,36 @@ export class AppComponent {
     this.initApp();
   }
 
-  initApp(){
-    this.platform.ready().then( async() => {
+  initApp() {
+    this.platform.ready().then(async () => {
       const lang = await Device.getLanguageCode();
       const info = await Device.getInfo();
 
       this.isWeb = info.platform == 'web';
-      
-      if (lang.value){
+
+      if (lang.value) {
         this.translate.use(lang.value.slice(0, 2));
       }
 
       this.sqliteService.init();
       this.sqliteService.dbReady.subscribe(isReady => {
-          this.load = isReady;
-          if (this.load){
-            this.alertServie.alertMessage(
-              this.translate.instant('Bienvenido'),
-              this.translate.instant('Que tengas buen entrenamiento')
-            );
+        this.load = isReady;
+        if (this.load) {
+          this.alertServie.alertMessage(
+            this.translate.instant('Bienvenido'),
+            this.translate.instant('Que tengas buen entrenamiento')
+          );
+        }
+      });
+      CapacitorApp.addListener('backButton', () => {
+        const currentUrl = this.router.url;
+        // Verificamos si estamos en el tab 2
+        if (currentUrl.includes('/tabs/tab2')) {
+          const confirmExit = confirm('¿Querés salir de la app?');
+          if (confirmExit) {
+            CapacitorApp.exitApp();
           }
+        }
       });
     });
   }
